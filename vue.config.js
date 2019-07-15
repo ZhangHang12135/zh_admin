f36f92e51ba4f311a127c0820b09973740700acb
ZhangHang12135 <z839187405@qq.com>
2019-07-15 10:15:17 +0800

    搭建整体环境，add iview && mockjs

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'procution' ? '/' : '/'
+// 检测是否是生产环境（打包），不是的话就是开发环境，直接在根目录下即可
+module.exports = {
+  lintOnSave: false,
+  publicPath: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))// 表示用@代表到src这一级的目录 ex: @/api = ..src/api
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时，不生成.map文件,加快打包速度，减少生成体积
+  productionSourceMap: false,
+  devServer: {
+    // proxy: 'http://localhost:3000'
+    // 告诉开发服务器，将任何未知请求，就是没有匹配到任何静态文件的请求都代理到这个URL,来满足跨域需求
+  }
+}
